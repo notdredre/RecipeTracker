3296f2f65171ee6e7af4dff76142d549ee90ad06
notdredre <87097301+notdredre@users.noreply.github.com>
2025-04-15 13:24:14 -0400

    Inits, reprs, and get_jsons

--- App/models/ingredient.py
+++ App/models/ingredient.py
@@ -4,3 +4,15 @@ from App.database import db
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f"ID: {self.id} Name: {self.name}"
+    
+    def get_json(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
