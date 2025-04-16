9189c4ebbcc766becfe04e35a5ea8d6350ee29ad
notdredre <87097301+notdredre@users.noreply.github.com>
2025-04-16 11:23:53 -0400

    Update UserInventory controllers

--- App/controllers/ingredient.py
+++ App/controllers/ingredient.py
@@ -67,6 +67,23 @@ def get_user_ingredient(user_id, ingredient_id):
     ingredient = db.session.query(Ingredient, UserInventory).filter(UserInventory.user_id == user_id, UserInventory.ingredient_id == ingredient_id).join(Ingredient, UserInventory.ingredient_id == Ingredient.id).first()
     return ingredient
 
+def delete_user_ingredient(user_id, ingredient_id):
+    if get_user_ingredient(user_id, ingredient_id):
+        _, inventory = get_user_ingredient(user_id, ingredient_id)
+        db.session.delete(inventory)
+        db.session.commit()
+        return True
+    return False
+
+def update_user_ingredient(user_id, ingredient_id, name, quantity):
+    if get_user_ingredient(user_id, ingredient_id):
+        ingredient, inventory = get_user_ingredient(user_id, ingredient_id)
+        ingredient.name = name
+        inventory.quantity = quantity
+        db.session.commit()
+        return True
+    return False
+
 def get_missing_ingredients(user_id, recipe_id):
     recipe_ingredients = get_recipe_ingredients(recipe_id)
     missing_ingredients = []
