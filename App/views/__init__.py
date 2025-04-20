2fff4a9ebe3ab5b7c594223f727cccf81b2c2811
notdredre <87097301+notdredre@users.noreply.github.com>
2025-04-20 10:29:27 -0400

    Add recipes blueprint to views init

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -4,7 +4,7 @@ from .user import user_views
 from .index import index_views
 from .auth import auth_views
 from .admin import setup_admin
+from .recipe import recipe_views
 
-
-views = [user_views, index_views, auth_views] 
+views = [user_views, index_views, auth_views, recipe_views] 
 # blueprints must be added to this list
