491a631ccbcc0fa99d333e86c51160911ca37ca8
notdredre <87097301+notdredre@users.noreply.github.com>
2025-04-20 11:15:56 -0400

    Add category to Recipe

--- App/models/recipe.py
+++ App/models/recipe.py
@@ -6,18 +6,20 @@ class Recipe(db.Model):
     name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.Text, nullable=True)
     steps = db.Column(db.Text, nullable=True)
+    category = db.Column(db.String(50), nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     ingredients = db.relationship("RecipeIngredient", backref="recipe", lazy=True)
 
 
-    def __init__(self, name, description, steps, user_id):
+    def __init__(self, name, description, steps, category, user_id):
         self.name = name
         self.description = description
         self.steps = steps
+        self.category = category
         self.user_id = user_id
 
     def __repr__(self):
-        return f"ID: {self.id} Name: {self.name} Description: {self.description} Steps: {self.steps} User ID: {self.user_id} Ingredients: {self.ingredients}"
+        return f"ID: {self.id} Name: {self.name} Description: {self.description} Steps: {self.steps} Category: {self.category} User ID: {self.user_id} Ingredients: {self.ingredients}"
     
     def get_json(self):
         return {
@@ -25,6 +27,7 @@ class Recipe(db.Model):
             "name": self.name,
             "description": self.description,
             "steps": self.steps,
+            "category": self.category,
             "user_id": self.user_id,
             "ingredients": [ingredient.get_json() for ingredient in self.ingredients]
         }
