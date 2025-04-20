73229901ee663b8d8e6cb08256b5a8834007bb17
notdredre <87097301+notdredre@users.noreply.github.com>
2025-04-20 13:25:25 -0400

    Adjusted ingredient routes to link to templates

--- App/views/ingredient.py
+++ App/views/ingredient.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, render_template, redirect, url_for
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from App.controllers.ingredient import (
     get_user_ingredients,
@@ -6,6 +6,8 @@ from App.controllers.ingredient import (
     add_user_ingredient,
     update_user_ingredient,
     delete_user_ingredient,
+    get_ingredient_by_name,
+    create_ingredient
 )
 
 ingredient_bp = Blueprint("ingredient", __name__, url_prefix="/ingredients")
@@ -15,26 +17,30 @@ ingredient_bp = Blueprint("ingredient", __name__, url_prefix="/ingredients")
 @jwt_required()
 def get_ingredients():
     user_id = get_jwt_identity()
-    ingredients = get_user_ingredients(user_id)
-    return jsonify(
-        [
-            {"ingredient": ingredient.get_json(), "inventory": inventory.get_json()}
-            for ingredient, inventory in ingredients
-        ]
-    )
+    ingredients = []
+    if get_user_ingredients(user_id):
+        ingredients = get_user_ingredients(user_id)
+    return render_template("ingredients.html", ingredients=ingredients)
 
 
 @ingredient_bp.route("/", methods=["POST"])
 @jwt_required()
 def add_ingredient():
     user_id = get_jwt_identity()
-    data = request.get_json()
-    ingredient_id = data.get("ingredient_id")
+    data = request.form
+    name = data.get("name")
     quantity = data.get("quantity")
-    if not ingredient_id or not quantity:
+    if not name or not quantity:
         return jsonify({"error": "Ingredient ID and quantity are required"}), 400
-    add_user_ingredient(user_id, ingredient_id, quantity)
-    return jsonify({"message": "Ingredient added successfully"})
+    ingredient = get_ingredient_by_name(name)
+    if ingredient:
+        if get_user_ingredient(user_id, ingredient.id):
+            return redirect(url_for("ingredient.update_ingredient", ingredient_id=ingredient.id))
+        add_user_ingredient(user_id, ingredient.id, quantity)
+    else:
+        new_ingredient = create_ingredient(name)
+        add_user_ingredient(user_id, new_ingredient.id, quantity)
+    return redirect(request.referrer)
 
 
 @ingredient_bp.route("/<int:ingredient_id>", methods=["GET"])
